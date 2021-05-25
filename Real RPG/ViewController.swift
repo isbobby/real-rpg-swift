4ea8752fe766b299ece686cde548c89352ce4396
isbobby <bobbyclex8@gmail.com>
2021-05-26 02:11:58 +0800

    Initial Commit

---
+++ Real RPG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Real RPG
+//
+//  Created by Leipetus  Hood  on 26/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
